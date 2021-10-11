68fc935e04b597c7189b6500942a8c6f890b40ca
Chungqvptit <85786862+Chungqvptit@users.noreply.github.com>
2021-10-11 10:47:22 +0700

    update code

--- bai1.rb
+++ bai1.rb
@@ -5,7 +5,9 @@ params = {"supplier" => {"name" => "Test lay params",
     "bank_accounts" => {"0" => {"account" => "847445", "branch_name" => "VCB"}, 
     "1" => {"account" => "985784", "branch_name" => "ACB", "_destroy" => "false"}}}}
 # a
-puts params["supplier"]["bank_accounts"]["1"]["branch_name"]
+# puts params["supplier"]["bank_accounts"]["1"]["branch_name"]
+puts params.dig("supplier", "bank_accounts", "1", "branch_name")
 #b
-params["type"] = "personal"
+# params["type"] = "personal"
+params.merge("type" => "personal")
 puts params
--- bai2.rb
+++ bai2.rb
@@ -8,17 +8,10 @@ puts b.include? c
 # b
 puts "b) "
 # function get intersection of a and b
-def array_common arr1, arr2
-    arr = arr1 & arr2
-    puts "intersection of a and b: #{arr}"
-end
-array_common a, b
+puts "intersection of a and b: #{a & b}"
 # c
 puts "c) "
 # function get all elements that are present in a and not present in b
-def array_diff arr1, arr2
-    arr = arr1 - arr2
-    puts "get all elements that are present in a and not present in b: #{arr}"
-end
-array_diff a, b
+puts "get all elements that are present in a and not present in b: #{a - b}"
+
  
